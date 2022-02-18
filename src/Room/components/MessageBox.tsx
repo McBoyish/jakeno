4818289578b5e4a029920affac61ab5c159609b1
jake <jake@eventdesk.co>
2022-02-18 14:09:05 -0500

    test

--- src/Room/components/MessageBox.tsx
+++ src/Room/components/MessageBox.tsx
@@ -60,6 +60,7 @@ export default function MessageBox({ messages, scrollRef }: MessageBoxProps) {
         ref={scrollRef}
         inverted
         disableVirtualization // this makes inverted work lol
+        keyboardShouldPersistTaps={'always'}
       />
     </View>
   );
