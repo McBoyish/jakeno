9bdc7c09fed7570aa18a241c2f4551c9f1772b0a
jake <jake@eventdesk.co>
2023-03-15 14:45:15 -0400

    /df.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,7 +29,7 @@ export default class MyDocument extends Document {
 
 	render() {
 		return (
-			<Html style={{ height: '100vh' }}>
+			<Html style={{ height: '100%' }}>
 				<Head>
 					<title>{'Jakeno'}</title>
 					<link rel='icon' href='/ufo.png' />
