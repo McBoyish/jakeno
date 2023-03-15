be932dcba3307312dae44dcd872466a5ab68b80e
jake <jake@eventdesk.co>
2023-03-15 00:38:55 -0400

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,19 @@ import type { AppProps } from 'next/app';
 import React from 'react';
 import { Portal } from 'react-native-paper';
 import SEO from 'src/common/SEO';
+import { Provider as PaperProvider } from 'react-native-paper';
+import theme from 'theme';
+import { UserContextProvider } from 'src/common/context/UserContext';
 
 function App({ Component, pageProps }: AppProps) {
 	return (
 		<Portal.Host>
-			<SEO />
-			<Component {...pageProps} />
+			<PaperProvider theme={theme}>
+				<UserContextProvider>
+					<SEO />
+					<Component {...pageProps} />
+				</UserContextProvider>
+			</PaperProvider>
 		</Portal.Host>
 	);
 }
--- src/common/Layout.tsx
+++ src/common/Layout.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
-import theme from 'theme';
-import { UserContextProvider } from 'src/common/context/UserContext';
 import NavBar from 'src/common/NavBar';
 import { ScrollView, View } from 'react-native';
 
@@ -13,24 +10,16 @@ interface LayoutProps {
 function Layout({ children, scrollView }: LayoutProps) {
 	if (scrollView)
 		return (
-			<PaperProvider theme={theme}>
-				<UserContextProvider>
-					<ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-						<NavBar />
-						{children}
-					</ScrollView>
-				</UserContextProvider>
-			</PaperProvider>
+			<ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+				<NavBar />
+				{children}
+			</ScrollView>
 		);
 	return (
-		<PaperProvider theme={theme}>
-			<UserContextProvider>
-				<View style={{ flexGrow: 1 }}>
-					<NavBar />
-					{children}
-				</View>
-			</UserContextProvider>
-		</PaperProvider>
+		<View style={{ flexGrow: 1 }}>
+			<NavBar />
+			{children}
+		</View>
 	);
 }
 
