edfd1912a692d3f86de80cc6288fa796cbcd806a
jake <jake@eventdesk.co>
2023-03-15 00:37:52 -0400

    fix

--- pages/room/[roomName].tsx
+++ pages/room/[roomName].tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import Layout from 'src/common/Layout';
 import Room from '../../src/Room';
 
 export default function RoomPage() {
-	return <Room />;
+	return (
+		<Layout>
+			<Room />
+		</Layout>
+	);
 }
